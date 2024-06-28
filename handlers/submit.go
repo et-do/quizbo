72aab45458dd713c94b1cb0113169740512fb3fe
Ethan Domokos <82214312+et-do@users.noreply.github.com>
2024-06-27 19:56:11 -0700

    updated submit handler
    expects json now in preps for react front end

--- handlers/submit.go
+++ handlers/submit.go
@@ -1,11 +1,9 @@
 package handlers
 
 import (
-	"html/template"
+	"encoding/json"
 	"log"
 	"net/http"
-
-	"read-robin/utils"
 )
 
 // URLRequest is a struct to hold the URL submitted by the user
@@ -19,49 +17,42 @@ type Response struct {
 	URL    string `json:"url"`
 }
 
-// SubmitHandler handles the form submission and responds with a template
+// SubmitHandler handles the form submission and responds with JSON
 func SubmitHandler(w http.ResponseWriter, r *http.Request) {
-	// Parse the form data from the request
-	// r.ParseForm() parses the form data and populates r.Form and r.PostForm
-	if err := r.ParseForm(); err != nil {
-		// If parsing the form fails, return a 400 Bad Request error
-		http.Error(w, "Unable to parse form", http.StatusBadRequest)
-		return
-	}
+	var urlRequest URLRequest
 
-	// Retrieve the URL value from the form data
-	// r.FormValue("url") returns the first value for the named component of the query
-	url := r.FormValue("url")
-	if url == "" {
-		// If the URL is empty, return a 400 Bad Request error
-		http.Error(w, "Missing URL", http.StatusBadRequest)
-		return
+	// Check the Content-Type header to determine how to parse the request body
+	if r.Header.Get("Content-Type") == "application/json" {
+		// Parse JSON data
+		if err := json.NewDecoder(r.Body).Decode(&urlRequest); err != nil {
+			http.Error(w, "Unable to parse JSON request", http.StatusBadRequest)
+			return
+		}
+	} else {
+		// Parse form data
+		if err := r.ParseForm(); err != nil {
+			http.Error(w, "Unable to parse form", http.StatusBadRequest)
+			return
+		}
+
+		urlRequest.URL = r.FormValue("url")
+		if urlRequest.URL == "" {
+			http.Error(w, "Missing URL", http.StatusBadRequest)
+			return
+		}
 	}
 
 	// Log the received URL to the server logs
-	log.Printf("Received URL: %s\n", url)
+	log.Printf("Received URL: %s\n", urlRequest.URL)
 
 	// Create a Response struct with the status and URL
-	response := Response{Status: "success", URL: url}
+	response := Response{Status: "success", URL: urlRequest.URL}
 
-	// Get the path to the template file
-	tmplPath := utils.GetTemplatePath("index.html")
-	// Parse the template file
-	tmpl, err := template.ParseFiles(tmplPath)
-	if err != nil {
-		// If parsing the template fails, log the error and return a 500 Internal Server Error
-		log.Printf("Error parsing template: %v", err)
-		http.Error(w, "Error loading template", http.StatusInternalServerError)
-		return
-	}
-
-	// Set the content type of the response to HTML
-	w.Header().Set("Content-Type", "text/html")
-	// Execute the template with the response data
-	err = tmpl.Execute(w, response)
-	if err != nil {
-		// If executing the template fails, log the error and return a 500 Internal Server Error
-		log.Printf("Error executing template: %v", err)
-		http.Error(w, "Error rendering template", http.StatusInternalServerError)
+	// Set the content type of the response to JSON
+	w.Header().Set("Content-Type", "application/json")
+	// Write the JSON response
+	if err := json.NewEncoder(w).Encode(response); err != nil {
+		log.Printf("Error encoding response: %v", err)
+		http.Error(w, "Error encoding response", http.StatusInternalServerError)
 	}
 }
