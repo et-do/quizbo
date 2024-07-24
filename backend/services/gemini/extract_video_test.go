b5757ab6419c1b0b6d915147b477258ee3ebaa13
Ethan Domokos <ethan.domokos@gmail.com>
2024-07-24 08:40:48 -0700

    added video front and backend

---
+++ backend/services/gemini/extract_video_test.go
@@ -0,0 +1,46 @@
+package gemini
+
+import (
+	"context"
+	"fmt"
+	"read-robin/models"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+const (
+	videoPath = "gs://read-robin-2e150.appspot.com/video/Happiness_ a very short story.mp4"
+)
+
+func TestExtractContentFromVideo(t *testing.T) {
+	ctx := context.Background()
+	client, err := NewGeminiClient(ctx)
+	assert.NoError(t, err)
+
+	video_path := videoPrompt{videoPath: videoPath}
+
+	contentMap, fullText, err := client.ExtractContentFromVideo(ctx, video_path.videoPath)
+	fmt.Print(contentMap)
+	fmt.Print(fullText)
+	assert.NoError(t, err)
+	assert.NotEmpty(t, contentMap)
+}
+
+func TestGenerateQuizFromVideo(t *testing.T) {
+	ctx := context.Background()
+	client, err := NewGeminiClient(ctx)
+	assert.NoError(t, err)
+
+	persona := models.Persona{
+		ID:         "Test_ID",
+		Name:       "Test Persona",
+		Role:       "Test Role",
+		Language:   "English",
+		Difficulty: "Easy"}
+
+	quizContent, err := client.GenerateQuizFromVideo(ctx, videoPath, persona)
+	fmt.Print(quizContent)
+	assert.NoError(t, err)
+	assert.NotEmpty(t, quizContent)
+}
