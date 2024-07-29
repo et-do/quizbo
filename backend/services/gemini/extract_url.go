53135b071fbcfea311600de4d628a9e2ba0ea9e6
Ethan Domokos <ethan.domokos@gmail.com>
2024-07-29 07:58:49 -0700

    generate quiz now stores "content_text" in firestore

--- backend/services/gemini/extract_url.go
+++ backend/services/gemini/extract_url.go
@@ -7,7 +7,7 @@ import (
 )
 
 const (
-	webscrapeModelSystemInstructions = `You are a highly skilled model that extracts readable text from HTML content and generates a title for the content. Your task is to extract the given HTML content and output it into a clear and concise article, ignoring any unnecessary HTML tags or irrelevant content. Additionally, generate a title from the URL to objectively define the site's host and page names (e.g., www.example.com would be Example, and https://en.wikipedia.org/wiki/The_World%27s_Largest_Lobster would be Wikipedia - The World's Largest Lobster). Return everything in a JSON dictionary with 'content' and 'title' keys. Exclude any markdown code fences in your response. The structure should look like this:
+	webscrapeModelSystemInstructions = `You are a highly skilled model that extracts the full readable text from HTML content and generates a title for the content. Your task is to extract the given HTML content and output it into a clear and concise article, ignoring any unnecessary HTML tags or irrelevant content. Additionally, generate a title from the URL to objectively define the site's host and page names (e.g., www.example.com would be Example, and https://en.wikipedia.org/wiki/The_World%27s_Largest_Lobster would be Wikipedia - The World's Largest Lobster). Return everything in a JSON dictionary with 'content' and 'title' keys. Exclude any markdown code fences in your response. The structure should look like this:
 {
 	"content": "extracted content",
 	"title": "generated title"
