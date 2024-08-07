e3444eba2f876316e70d469b6c22520dfec22bc6
Ethan Domokos <ethan.domokos@gmail.com>
2024-08-07 08:01:27 -0700

    renamed react app

---
+++ frontend/quizbo-app/src/About.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./App.css";
+
+function About() {
+  return (
+    <div className="about-page">
+      <h1>About Quizbo</h1>
+      <p>Quizbo is your AI companion for smarter comprehension...</p>
+    </div>
+  );
+}
+
+export default About;
