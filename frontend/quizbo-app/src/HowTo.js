e5b09e14eb792a3a95d65a80bb44465961970faf
Ethan Domokos <ethan.domokos@gmail.com>
2024-08-07 08:01:27 -0700

    renamed react app

---
+++ frontend/quizbo-app/src/HowTo.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./App.css";
+
+function HowTo() {
+  return (
+    <div className="howto-page">
+      <h1>How to Use Quizbo</h1>
+      <p>Instructions on how to use the Quizbo app...</p>
+    </div>
+  );
+}
+
+export default HowTo;
